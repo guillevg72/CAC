fbd628c072bb24c55c7c42cb5179549c3d30e2a7
guillevg72 <guillevg72@gmail.com>
2022-10-26 21:42:46 -0300

    Validaciones de nombre, apellido, cantidad y email

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,6 @@
+let costo=0;
+
 function calCosto(){
-    let costo=0;
     let total=0;
     let cantidad = parseInt(document.getElementById("cantidad").value);
     let categoria = document.getElementById("categoria").value;
@@ -7,23 +8,45 @@ function calCosto(){
     let varsurname = document.getElementById("surname").value;
     let varemail = document.getElementById("email").value;
 
-
-    if (categoria==1){
-            total=cantidad*0.2*200;
-            costo=total.toFixed();
-    } 
-    else { 
-        if (categoria==2) {
-            total=cantidad*0.5*200;
-            costo=total.toFixed();
-        } 
-        else {
-            total=cantidad*0.85*200;
-            costo=total.toFixed();
-        }
+    if(varname==""){
+        alert("Ingrese su nombre");
+        return;
     }
+    if(varsurname==""){
+        alert("Ingrese su apellido");
+        return;
+    }
+    if(varemail==""){
+        alert("Ingrese su email");
+        return;
+    }        
+     
+    if(isNaN(cantidad)){
+        alert("Ingrese una cantidad valida de Tickets");
+        return;
+    }
+
+    if (categoria==0){
+        total=cantidad*200;
+        costo=total.toFixed();
+    } 
+    else if (categoria==1) {
+        total=cantidad*0.2*200;
+        costo=total.toFixed();
+    } 
+    else if (categoria==2) {
+        total=cantidad*0.5*200;
+        costo=total.toFixed();
+    } 
+    else {
+        total=cantidad*0.85*200;
+        costo=total.toFixed();
+    }
+    
+    
     document.getElementById('resultado').innerHTML = costo
     console.log(costo);
+    console.log(cantidad);
     console.log(categoria);
    
 }
