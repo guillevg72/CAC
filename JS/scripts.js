8977958e91d63754c53b85bc63fcd50224398227
guillevg72 <guillevg72@gmail.com>
2022-10-28 17:56:29 -0300

    Agregado de validacion cantidad de entradas diferente de 0

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,48 +1,49 @@
-bt_resumen.addEventListener('click',function calCosto(){
+    bt_resumen.addEventListener('click',function calCosto(){
+    let ticket=200;
     let costo=0;
     let total=0;
     let cantidad = parseInt(document.getElementById("cantidad").value);
     let categoria = document.getElementById("categoria").value;
-    let varname = document.getElementById("name").value;
-    let varsurname = document.getElementById("surname").value;
-    let varemail = document.getElementById("email").value;
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let email = document.getElementById("email").value;
     let validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
-    if(varname==""){
+    if(name==""){
         alert("Ingrese su nombre");
+        name.classList.add("is-invalid");
+        name.focus();
        return;
     }
-    if(varsurname==""){
+    if(surname==""){
         alert("Ingrese su apellido");
        return;
     }
     
-    if( !validEmail.test(varemail) ){ 
+    if( !validEmail.test(email) ){ 
         alert('Ingrese un email valido');
         return;
 	}
 
-
-    
-    if(isNaN(cantidad)){
+    if(isNaN(cantidad)||cantidad==0){
         alert("Ingrese una cantidad valida de Tickets");
         return;
     }
 
     if (categoria==0){
-        total=cantidad*200;
+        total=cantidad*ticket;
         costo=total.toFixed();
     } 
     else if (categoria==1) {
-        total=cantidad*0.2*200;
+        total=cantidad*0.2*ticket;
         costo=total.toFixed();
     } 
     else if (categoria==2) {
-        total=cantidad*0.5*200;
+        total=cantidad*0.5*ticket;
         costo=total.toFixed();
     } 
     else {
-        total=cantidad*0.85*200;
+        total=cantidad*0.85*ticket;
         costo=total.toFixed();
     }
     
